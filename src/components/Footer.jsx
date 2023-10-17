a03632b06f0b45c04a78607f80e6c02b35a343dc
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-10-16 20:43:35 -0400

    footer css refactor. view height and width corrected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,24 @@
 import { Link, useLocation } from 'react-router-dom';
 import { FaGithubSquare, FaLinkedin, FaHeart } from 'react-icons/fa'
 
+import './footer.css'
+
 function Footer() {
     const currentPage = useLocation().pathname;
 
     return (
-        <div className="footer-icons">
-            <a className="footerLinks" href = "https://github.com/jtdoaks"  target='_blank'><FaGithubSquare/></a> 
-            <a className="footerLinks" href = "https://linkedin.com/in/john-doaks-86028b267"  target='_blank'><FaLinkedin/></a> 
-            <a className="footerLinks" href = "https://stuffonmycat.com/"  target='_blank'><FaHeart/></a>
-        </div>);
+
+        <div className = "footer">
+
+
+            <div className="footer-icons">
+                <a className="footerLinks" href="https://github.com/jtdoaks" target='_blank'><FaGithubSquare /></a>
+                <a className="footerLinks" href="https://linkedin.com/in/john-doaks-86028b267" target='_blank'><FaLinkedin /></a>
+                <a className="footerLinks" href="https://stuffonmycat.com/" target='_blank'><FaHeart /></a>
+            </div>
+
+        </div>
+    );
 
 }
 
