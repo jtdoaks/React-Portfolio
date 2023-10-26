b3876e546d62712327f27eaa2b2d491e02c91dac
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-10-26 19:57:48 -0400

    resized images. placed home text in the center of thhe images. added labels to the photos that chnage withthe photos. media query so when the screen shrinks the text displays on top of the images.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,21 @@ import fieldIowa from "../assets/fieldIowa.png";
 import './home.css'
 
 const homeImages = [
- coverPhoto,
- bigRock,
- skippingRocks,
- throwing,
- fieldIowa
+  coverPhoto,
+  bigRock,
+  skippingRocks,
+  throwing,
+  fieldIowa
 
 ]
+
+const imageLabels = [
+  "Redwood State Forest, September 2018",
+  "Big Rock, Crescent City California, February 2020",
+  "Skipping Stones, Six Rivers National Forest, September 2018",
+  "Throwing Rocks at Shilsole Bay, Washington, March 2023",
+  "A Feild, Iowa, Septmeber 2017"
+];
 
 export default function About() {
 
@@ -24,7 +32,7 @@ export default function About() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      
+      // Move to the next image in the array
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % homeImages.length);
     }, 3000); // Change images every 3 seconds 
 
@@ -34,20 +42,27 @@ export default function About() {
   return (
     <div className="home-root">
       <div className="home-body">
-        <h5 className="home-title">About Me</h5>
-        <p className="about-text">This is about John. Something about how I love plants and also coding. Idk dichotomy. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptate cum provident, libero voluptatibus nesciunt repellendus fugit voluptas, beatae id, ad quasi expedita omnis totam obcaecati distinctio. Mollitia, ducimus incidunt.</p>
+        <div className="homeText">
+          <div className="col-md-8 order-1 order-md-2">
+            <div className="home-body">
+              <h5 className="home-title">About Me</h5>
+              <p className="about-text">This is about John. Something about how I love plants and also coding. Idk dichotomy. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptate cum provident, libero voluptatibus nesciunt repellendus fugit voluptas, beatae id, ad quasi expedita omnis totam obcaecati distinctio. Mollitia, ducimus incidunt.</p>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="col-md-8 order-2 order-md-1">
 
-      <div className="homeImgDiv">
-        <div className="image-container">
-          {homeImages.map((image, index) => (
+        <div className="homeImgDiv">
+          <div className="image-container">
+
             <img
-              key={index}
-              className={`home-img ${index === currentImageIndex ? "visible" : ""}`}
-              src={image}  
-              alt={`Image ${index + 1}`}
+              className="home-img"
+              src={homeImages[currentImageIndex]}
+              alt={`Image ${currentImageIndex + 1}`}
             />
-          ))}
+            <p className="imageLabel">{imageLabels[currentImageIndex]}</p>
+          </div>
         </div>
       </div>
     </div>
